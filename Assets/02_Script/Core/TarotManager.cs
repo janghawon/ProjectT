5f17644c2cea816d30293ebb156a85be7195ef44
janghawon <98999600+janghawon@users.noreply.github.com>
2024-06-30 13:31:46 +0900

    0630

---
+++ Assets/02_Script/Core/TarotManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TarotManager : MonoSingleton<TarotManager>
+{
+    [SerializeField] private TarotCardInfo[] _tarotContainer;
+    public TarotCardInfo[] TarotContainer => _tarotContainer;
+
+    private Dictionary<int, TarotCardInfo> _tarotCardDic = new();
+
+    private void Awake()
+    {
+        foreach (var tarot in _tarotContainer)
+        {
+            _tarotCardDic.Add(tarot.id, tarot);
+        }
+    }
+
+    public TarotCardInfo GetTarotInfoByID(int tarotID)
+    {
+        if(!_tarotCardDic.ContainsKey(tarotID))
+        {
+            Debug.LogError($"Error : Not Found Tarot Info By ID : {tarotID}");
+            return null;
+        }
+
+        return _tarotCardDic[tarotID];
+    }
+}
